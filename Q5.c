feff32f4a24f90c78ed312330f9355ace911176b
Marcos Melo <124604286+TheMarksan@users.noreply.github.com>
2023-08-16 08:56:51 -0300

    Questao 5

---
+++ Q5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int resto, n1, n2;
+    scanf("%d%d", &n1, &n2);
+
+    while(n2!=0){
+        resto = n1%n2;
+        n1 = n2;
+        n2 = resto;
+    }
+    printf("%d\n", n1);
+    return 0;
+}
