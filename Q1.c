3d55c20494a11a97099783cd73a5fbec634ae748
Kcodesufal <142046371+Kcodesufal@users.noreply.github.com>
2023-08-21 21:24:45 +0000

    Primeira feita

--- Q1.c
+++ Q1.c
@@ -1,2 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
+//  Escrever um programa que determina se um dado número inteiro é primo ou não.
+int main () {
+
+   unsigned int N, cont, time;
+scanf ("%d", &N);
+time = clock();
+if ( (N%2 == 0 && N != 2) || N <= 1)
+{
+printf ("NÃO PRIMO");
+return 0;
+}
+    for (cont = 3; cont <= sqrt(N); cont+= 2)
+    {
+        if (N%cont == 0)
+        {
+            printf ("NÃO PRIMO");
+            return 0;
+        }
+          
+    }
+    printf ("\nPRIMO");
+    printf("%ld\n", clock()-time);
+
+    return 0;
+
+   }
