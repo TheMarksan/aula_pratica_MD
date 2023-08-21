4a0cc5141e906e46de80b281940505f33678cbb3
Kcodesufal <142046371+Kcodesufal@users.noreply.github.com>
2023-08-21 09:03:32 -0300

    a

--- Q4.c
+++ Q4.c
@@ -3,3 +3,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+void primemaker (int A, int B)
+{
+while (A != 1)
+    while (A % 2 == 0) {
+        A = A/2;
+        printf ("2\n");
+    }
+
+   n = 3;
+   while (A%n !=0)
+   {
+    n++
+   }
+   while (A%n == 0)
+   {
+    A = A/n;
+    printf ("%d\n" n);
+   }
+
+int main () {
+
+int A, B;
+
+scanf ("%d %d", &A, &B);
+primemaker (A, B);
+
+
+
+
+
+
+
+
+
+}
